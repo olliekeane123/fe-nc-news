70a7822c38b1f29f596209a8ad49e0ea40893a7c
olliekeane123 <ollie.keane1@gmail.com>
2024-12-20 09:55:18 +0000

    add topics function for main page, made start on explore page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
+import { getArticles } from './api'
 import { Routes, Route } from 'react-router-dom'
 import { Home } from './components/Home'
 import { Explore } from './components/Explore'
@@ -12,18 +13,31 @@ import { IndividualArticle } from './components/IndividualArticle'
 function App() {
 
   const [articles, setArticles] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  
+
+    useEffect(() => {
+        setIsLoading(true)
+        getArticles().then((response) => {
+            setArticles(response)
+            setIsLoading(false)
+        })
+    }, [])
 
   return (
   <>
       <Header className='header'/>
       <main className='main'>
         <Routes>
-          <Route path='/' element={<Home />}/>
-          <Route path='/explore' element={<Explore articles={articles} setArticles={setArticles}/>}/>
+          <Route path='/' element={<Home articles={articles} isLoading={isLoading}/>}/>
+          <Route path='/explore' element={<Explore articles={articles} isLoading={isLoading} setArticles={setArticles} />}/>
+          <Route path='/explore/:topic' element={<Explore articles={articles} isLoading={isLoading}/>}/>
+          <Route path='/explore/:sort_by' element={<Explore articles={articles} isLoading={isLoading}/>}/>
+          <Route path='/explore/:order' element={<Explore articles={articles} isLoading={isLoading}/>}/>
           <Route path='/post' element={<Post />}/>
           <Route path='/login' element={<LogIn />}/>
           <Route path='/account/:username' element={<Account />}/>
-          <Route path='/explore/:articleId' element={<IndividualArticle />}/>
+          <Route path='/explore/article/:articleId' element={<IndividualArticle />}/>
         </Routes>
       </main>
   </>
